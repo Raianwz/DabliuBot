87e4f4055eb81ced2f6ebed0c78e767923aa6332
Raianwz <Raian03@live.com>
2021-12-11 17:15:26 -0300

    Update emotes
    Pequena atualização nos emotes do bot.

--- comandos/dados.js
+++ comandos/dados.js
@@ -8,6 +8,7 @@ module.exports.run = async (client, message, args) => {
     const user = message.author;
     const val = ['-1', '0', '+1']
     let atributo = 0, calc = 0;
+    let emote = "<:HmmNote:919290616490360852>"
 
     //filtrando argumentos
     if (args.length == 1) {
@@ -18,7 +19,7 @@ module.exports.run = async (client, message, args) => {
         for (let i = 0; i < args.length; i++) {
             calc += Number(args[i])
             if (isNaN(calc)) {
-                return message.channel.send(`<:peepoClown:858694142124752917> ${user.toString()} Digite apenas números e evite espaços entre + e o - em um número(+1,-1).`)
+                return message.channel.send(`🤡 ${user.toString()} Digite apenas números e evite espaços entre os sinais de + e - em um número.`)
             }
         }
     }
@@ -27,12 +28,12 @@ module.exports.run = async (client, message, args) => {
     let rsp = Number(n1) + Number(n2) + Number(n3) + Number(n4) + atributo + calc
 
     if (atributo > 0) {
-        message.channel.send(`<:HmmNote:858691453513105429> ${user.toString()} Você rolou: [${n1}, ${n2}, ${n3}, ${n4}] com +${atributo}\n **Total: ${rsp}**`)
+        message.channel.send(`${emote} ${user.toString()} Você rolou: [${n1}, ${n2}, ${n3}, ${n4}] com +${atributo}\n **Total: ${rsp}**`)
     }
     else if (calc > 0) {
-        message.channel.send(`<:HmmNote:858691453513105429> ${user.toString()} Você rolou: [${n1}, ${n2}, ${n3}, ${n4}] com +${calc}\n **Total: ${rsp}**`)
+        message.channel.send(`${emote} ${user.toString()} Você rolou: [${n1}, ${n2}, ${n3}, ${n4}] com +${calc}\n **Total: ${rsp}**`)
     }
     else {
-        message.channel.send(`<:HmmNote:858691453513105429> ${user.toString()} Você rolou: [${n1}, ${n2}, ${n3}, ${n4}]\n **Total: ${rsp}**`)
+        message.channel.send(`${emote} ${user.toString()} Você rolou: [${n1}, ${n2}, ${n3}, ${n4}]\n **Total: ${rsp}**`)
     }
 };
